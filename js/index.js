3884f0cbb0a92251ed69a2125d730045fd448b56
Toshiki A <tarita141@gmail.com>
2016-10-30 17:40:44 -0700

    Update hero image, Update slider style and links, Add invest page

--- js/index.js
+++ js/index.js
@@ -12,8 +12,7 @@ $(document).ready(function () {
     speed: 800,
     slidesToShow: 3,
     slidesToScroll: 3,
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -21,19 +20,19 @@ $(document).ready(function () {
           infinite: true,
           dots: true
         }
-      },
-      {
+      }, {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
-      },
-      {
+      }, {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
         }
       }
       // You can unslick at a given breakpoint now by adding:
